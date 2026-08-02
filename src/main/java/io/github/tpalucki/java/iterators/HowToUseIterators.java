8e0777a912c6e7fbe733059e0be5ca298f6b5252
Tomasz Pałucki <tpalucki@gmail.com>
2026-08-02 20:38:54 +0200

    feat: Iterators updated

--- src/main/java/io/github/tpalucki/java/iterators/HowToUseIterators.java
+++ src/main/java/io/github/tpalucki/java/iterators/HowToUseIterators.java
@@ -1,19 +1,25 @@
 package io.github.tpalucki.java.iterators;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class HowToUseIterators {
 
-    public static void main(String[] args) {
+    /**
+     * INFO: Default iterators for Collections from java.util package such as ArrayList, HashMap, etc. are Fail-Fast.
+     */
+    static void main(String[] args) {
         howToIterate();
 
         howToRemove();
 
         howToUseListIterator();
+
+        failSafeIterator();
     }
 
     private static void howToIterate() {
-        System.out.println("How to iterate with iterator?");
+        IO.println("How to iterate with iterator?");
         var nums = List.of(1, 2, 3);
 
         final Iterator<Integer> iterator = nums.iterator();
@@ -24,8 +30,9 @@ public class HowToUseIterators {
     }
 
     private static void howToRemove() {
-        System.out.println("How to remove with iterator");
-//        var nums = List.of(1, 2, 3, 4, 5); // na immutable kolekcji próba usunięcia zwróci java.lang.UnsupportedOperationException
+        IO.println("How to remove with iterator");
+//        var nums = List.of(1, 2, 3, 4, 5); // na immutable kolekcji próba usunięcia
+//        zwróci java.lang.UnsupportedOperationException
         var nums = new ArrayList<Integer>();
         nums.addAll(List.of(1, 2, 3, 4, 5));
 
@@ -35,21 +42,44 @@ public class HowToUseIterators {
             var next = iterator.next(); // <----- TUTAJ!!!
             if (next % 2 == 1) {
                 iterator.remove();
-                System.out.println("next = " + next + " - removed");
+                IO.println("next = " + next + " - removed");
             } else {
-                System.out.println("next = " + next);
+                IO.println("next = " + next);
             }
         }
 
         var newIterator = nums.iterator(); // tworzy nowy iterator
-        newIterator.forEachRemaining(i -> System.out.println("After removal next = " + i));
+        newIterator.forEachRemaining(i -> IO.println("After removal next = " + i));
     }
 
     private static void howToUseListIterator() {
         var nums = List.of(1, 2, 3, 4, 5);
 
         final ListIterator<Integer> listIterator = nums.listIterator();
-        listIterator.remove();
+        try {
+            listIterator.remove();
+            // throws UnsupportedOperationException because it has to be called once after next() or previous()
+        } catch (UnsupportedOperationException e) {
+            IO.println("ListIterator is unmodifiable");
+        }
+    }
 
+    private static void failSafeIterator() {
+        Map<String, Integer> sharedMap = new ConcurrentHashMap<String, Integer>();
+        sharedMap.put("First", 10);
+        sharedMap.put("Second", 20);
+        sharedMap.put("Third", 30);
+        sharedMap.put("Fourth", 40);
+
+        Iterator<String> iterator = sharedMap.keySet().iterator();
+
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            IO.println("Current key from shared map = " + key);
+            // !!! Inserting to collection while iterating!
+            // with fail-safe it's possible, iteration still completes but with added element
+            // it does detect the newly added element to the Collection.
+            sharedMap.put("Fifth", 50);
+        }
     }
 }
